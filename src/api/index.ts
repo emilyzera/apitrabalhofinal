896328133b89a4b3c6772d6756b49bd5b5167010
emily wust <emily.ufc18@gmail.com>
2023-04-30 23:27:29 -0300

    first commit

---
+++ src/api/index.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosResponse } from "axios";
+
+axios.defaults.baseURL = "http://localhost:3333";
+
+const apiGet = async (url: string) => {
+  const response: AxiosResponse = await axios.get(url);
+  return response;
+};
+
+const apiPut = async (url: string, data: any) => {
+  const response: AxiosResponse = await axios.put(url, data);
+  return response;
+};
+
+const apiPost = async (url: string, data: any) => {
+  const response: AxiosResponse = await axios.post(url, data);
+  return response;
+};
+
+const apiDelete = async (url: string) => {
+  const response: AxiosResponse = await axios.delete(url);
+  return response;
+};
+
+export { apiGet, apiPut, apiPost, apiDelete };
---
+++ src/store/modules/SliceRecados/SliceRecados.ts
@@ -0,0 +1,51 @@
+import {
+  createAsyncThunk,
+  createEntityAdapter,
+  createSlice,
+} from "@reduxjs/toolkit";
+import axios from "axios";
+import { EstadoDaStore } from "../..";
+
+export interface Recado {
+  id: string;
+  titulo: string;
+  descricao: string;
+  data: string;
+  usuario: string;
+}
+
+export const fetchrecados = createAsyncThunk(
+  "recado/fetchrecados",
+  async () => {
+    const result = await axios.get("https://emilywusttrabalho.onrender.com");
+    return result.data.data;
+  }
+);
+
+const adaptadorDosRecados = createEntityAdapter<Recado>({
+  selectId: (recado) => recado.id,
+});
+
+export const { selectAll, selectById } = adaptadorDosRecados.getSelectors(
+  (state: EstadoDaStore) => state.recados
+);
+
+const SliceRecado = createSlice({
+  name: "recado",
+  initialState: adaptadorDosRecados.getInitialState(), //
+  reducers: {
+    adicionarRecado: adaptadorDosRecados.addOne,
+    atualizarRecado: adaptadorDosRecados.updateOne,
+    removerRecado: adaptadorDosRecados.removeOne,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchrecados.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
+});
+
+export const { adicionarRecado, atualizarRecado, removerRecado } =
+  SliceRecado.actions;
+
+export default SliceRecado.reducer;
